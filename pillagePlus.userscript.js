6858d4e44082ecb93db46ea31411d8f59f7c471e
Lodis <mborgeat@hotmail.ch>
2016-02-23 21:38:34 +0100

    Starting

---
+++ pillagePlus.userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         pillagePlus
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Facilitez vous la vie avec vos pillages
+// @author       Lodis
+// @match        http://game.asylamba.com/*
+// @require	http://code.jquery.com/jquery-2.1.1.min.js
+// @grant        none
+// ==/UserScript==
+
+
